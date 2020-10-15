44e091b0855fa1ddbf8bd411ff7143f6e666d9e9
Juan <meltedpiface50@gmail.com>
2020-10-15 12:01:09 -0400

    Products

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'> Copyright &copy; KnickKnacksd</Col>
+          <Col className='text-center py-3'> Copyright &copy; KnickKnacks</Col>
         </Row>
       </Container>
     </footer>
