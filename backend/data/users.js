49dfbb13e0241c82af22c1ccb5c721ffce97175b
Juan <juniorguzman02@hotmail.com>
2020-10-22 00:13:07 -0400

    created users

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: xxxx,
+    isAdmin: true,
+  },
+  {
+    name: "John Cena",
+    email: "john@example.com",
+    password: xxxx,
+  },
+  {
+    name: "Mary Jane",
+    email: "mary@example.com",
+    password: xxxx,
+  },
+];
