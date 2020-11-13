2a161881a371f1e8b21c2166d8b1ca51604663fe
Juan <juniorguzman02@hotmail.com>
2020-11-13 14:52:14 -0500

    Users can now register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,4 +45,37 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+//@desc Register user
+//@route POST /api/users
+//@access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
