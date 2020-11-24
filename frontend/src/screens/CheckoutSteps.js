5fad61dcebb972b736d99290bb8bf48c903c194c
juan <juniorguzman02@hotmail.com>
2020-11-23 22:53:14 -0500

    Checkout Steps Test

---
+++ frontend/src/screens/CheckoutSteps.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+
+const CheckoutSteps = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CheckoutSteps
